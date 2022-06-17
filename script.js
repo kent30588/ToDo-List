5125212819d7c0a12b06b0cfa56c3b24c3c45283
unknown <ak4raulov@yandex.ru>
2022-06-17 16:36:20 +0300

    добавил проверку на схожие задания

--- script.js
+++ script.js
@@ -8,21 +8,41 @@ const tasks = [];
 
 // Отслеживаем клие по кнопке
 dom.add.onclick = () => {
-    const task = dom.new.value;
-    if (task) {
-        addTask(task);
+    const newTaskText = dom.new.value;
+    if (newTaskText && isNotHaveTask(newTaskText, tasks)) {
+        addTask(newTaskText, tasks);
     }
+    dom.new.value = '';
 };
 
 // функция добавления задач
 
-function addTask(text) {
+function addTask(text, list) {
     const timestamp = Date.now();
     const task = {
         id: timestamp,
         text,
         isComplete: false
     };
-    tasks.push(task);
-    console.log(tasks);
+    list.push(task);
+}
+
+// Проверка на схожие задачи
+
+function isNotHaveTask(text, list) {
+    let isNotHave = true;
+
+    list.forEach((task) => {
+        if (task.text === text) {
+            alert('Такая задача уже существует');
+            isNotHave = false;
+        }
+    });
+    return isNotHave;
+}
+
+// Функция вывода списка задач
+
+function tasksRender(list) {
+
 }
